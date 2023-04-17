cebc52c65241dd41eeb3306fcc990d332e71136c
Thiri <thirihtet246@gmail.com>
2023-04-17 11:36:51 +0630

    Add private method

---
+++ person.rb
@@ -0,0 +1,23 @@
+class Person
+    attr_reader :id
+    attr_accessor :name, :age
+    def initialize(name = "Unknown", age, parent_permission: true)
+        @id = Random.rand(1..1000)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+    private
+
+    def of_age?
+        return true if @age >= 18
+        return false
+    end
+
+    private :of_age?
+
+end
+
+person = Person.new(15)
+puts person.instance_eval('of_age?')
