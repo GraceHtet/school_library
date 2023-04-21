abb568192a2f7085ca228289b360e0da384a0af5
Thiri <thirihtet246@gmail.com>
2023-04-21 13:30:01 +0630

    Add basic ui

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
   def play_hooky
