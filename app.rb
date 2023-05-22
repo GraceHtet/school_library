880ae6fa9e3aa26d5dba97a0418484cf9566acb5
Thiri <thirihtet246@gmail.com>
2023-05-22 17:40:11 +0630

    Fix required changes

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require_relative './lib/teacher'
 require_relative './lib/rental'
 
 class Option
+  attr_reader :books, :person, :rents
+
   def initialize
     @books = []
     @person = []
@@ -25,71 +27,27 @@ class Option
     end
   end
 
-  def create_person
-    puts 'Do you want to create a student(1) or a teacher(2)? [Input the number]: '
-    type = gets.chomp.to_i
-
-    if [1, 2].include?(type)
-      print 'Age: '
-      age = gets.chomp.to_i
-
-      print 'Name: '
-      name = gets.chomp
-
-      if type == 1
-        print 'Has parent Permission? [Y/N]: '
-        permission = gets.chomp.downcase == 'y'
-        people = Student.new(age, name, parent_permission: permission)
-      elsif type == 2
-        print 'Specialization: '
-        people = Teacher.new(gets.chomp, age, name)
-      end
-      @person << people
-      puts "Created a Person successfully. \n"
-    else
-      puts "Invalid type \n"
+  def create_person(type, age, name, permission: true, specialization: '')
+    if type == 1
+      people = Student.new(age, name, parent_permission: permission)
+    elsif type == 2
+      people = Teacher.new(specialization, age, name)
     end
+    @person << people
+    puts "Created a Person successfully. \n"\
   end
 
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-
-    print 'Author: '
-    author = gets.chomp
-
+  def create_book(title, author)
     @books << Book.new(title, author)
     print 'Book created'
   end
 
-  def create_rental
-    puts 'Select a book from the following list by number: '
-    @books.each_with_index do |book, i|
-      puts "#{i}) Title: #{book.title}, Author: #{book.author}"
-    end
-    book_num = gets.chomp.to_i.to_i
-
-    puts 'Select a person from the following list by number (not id)'
-    @person.each_with_index do |each, i|
-      puts "#{i}) [#{each.class}] Name: #{each.name}, ID: #{each.id}, Age: #{each.age}"
-    end
-    person_num = gets.chomp.to_i.to_i
-
-    print 'Date: '
-    date = gets.chomp.to_i
-
+  def create_rental(date, person_num, book_num)
     @rents << Rental.new(date, @person[person_num], @books[book_num])
     print "Rental created successfully \n"
   end
 
-  def list_rentals
-    puts 'List all rents for a given person id'
-    person_id = gets.chomp.to_i.to_i
-    selected = @rents.select { |r| r.person.id == person_id }
-
-    puts "No rental found for this person id \n" if selected.empty?
-
-    puts 'Rental: '
+  def list_rentals(selected)
     selected.each do |select|
       puts "Date: #{select.date}, Book: \"#{select.book.title}\" by #{select.book.author}"
     end
--- menu.rb
+++ menu.rb
@@ -21,10 +21,92 @@ class Menu
   def handle_selection(selection)
     handlers = OPTIONS[selection]
     if handlers
-      @app.send(handlers[:action])
+      if (3..6).include?(selection)
+        choose_option(selection)
+      else
+        @app.send(handlers[:action])
+      end
     else
       puts 'Invalid options ):'
     end
+  end
+
+  def choose_option(option)
+    case option
+    when 3
+      option_three
+    when 4
+      option_four
+    when 5
+      option_five
+    when 6
+      option_six
+    end
+  end
+
+  def option_three
+    puts 'Do you want to create a student(1) or a teacher(2)? [Input the number]: '
+    type = gets.chomp.to_i
+
+    if [1, 2].include?(type)
+      print 'Age: '
+      age = gets.chomp.to_i
+
+      print 'Name: '
+      name = gets.chomp
+
+      case type
+      when 1
+        print 'Has parent Permission? [Y/N]: '
+        permission = gets.chomp.downcase == 'y'
+        @app.create_person(type, name, age, permission: permission)
+      when 2
+        print 'Specialization: '
+        specialization = gets.chomp
+        @app.create_person(type, name, age, specialization: specialization)
+      end
+    else
+      puts 'Type a valid option'
+    end
+  end
+
+  def option_four
+    print 'Title: '
+    title = gets.chomp
+
+    print 'Author: '
+    author = gets.chomp
+    @app.create_book(title, author)
+  end
+
+  def option_five
+    puts 'Select a book from the following list by number: '
+    @app.books.each_with_index do |book, i|
+      puts "#{i}) Title: #{book.title}, Author: #{book.author}"
+    end
+    book_num = gets.chomp.to_i
+
+    puts 'Select a person from the following list by number (not id)'
+    @app.person.each_with_index do |each, i|
+      puts "#{i}) [#{each.class}] Name: #{each.name}, ID: #{each.id}, Age: #{each.age}"
+    end
+    person_num = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp.to_i
+
+    @app.create_rental(date, person_num, book_num)
+  end
+
+  def option_six
+    puts 'List all rents for a given person id'
+    person_id = gets.chomp.to_i
+    selected = @app.rents.select { |r| r.person.id == person_id }
+
+    return puts "No rental found for this person id \n" if selected.empty?
+
+    puts 'Rental: '
+    @app.list_rentals(selected)
   end
 
   def menu_list
