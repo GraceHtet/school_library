59de346a7b70269731616190aafae5ae6c7d9631
zeff96 <zeffo1975@gmail.com>
2023-05-23 19:24:16 +0300

    Add hash method to convert to ruby hash.

--- lib/rental.rb
+++ lib/rental.rb
@@ -10,4 +10,12 @@ class Rental
     @book = book
     book.rents << self
   end
+
+  def to_hash
+    {
+      'date' => @date,
+      'book' => @book.to_hash,
+      'person' => @person.to_hash
+    }
+  end
 end
